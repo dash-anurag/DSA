8619cab4f383c7965885443bc39822b4b42f7907
Anurag Dash <anuragjeypore1234@gmail.com>
2021-03-22 00:10:07 +0530

    Implementation of Queues using Stack ( js )

---
+++ Queue-DataStructure/QueuesUsingStacks/main.js
@@ -0,0 +1,43 @@
+class CrazyQueue {
+  constructor() {
+    this.first = [];
+    this.last = [];
+  }
+  enqueue(value) {
+    const length = this.first.length;
+    for (let i = 0; i < length; i++) {
+      this.last.push(this.first.pop());
+    }
+    this.last.push(value);
+    console.log(this);
+    return this;
+  }
+  dequeue() {
+    const length = this.last.length;
+    for (let i = 0; i < length; i++) {
+      this.first.push(this.last.pop());
+    }
+    this.first.pop();
+    console.log(this);
+    return this;
+  }
+  peek() {
+    if (this.last.length > 0) {
+      console.log(this.last[0]);
+      return this.last[0];
+    }
+    console.log(this.first[this.first.length - 1]);
+    return this.first[this.first.length - 1];
+  }
+}
+
+const myQueue = new CrazyQueue();
+myQueue.peek();
+myQueue.enqueue("Anurag");
+myQueue.enqueue("Pritish");
+myQueue.enqueue("Swapnil");
+myQueue.peek();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.peek();
